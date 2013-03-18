9c3f1650a448ed3340a9a2bdc3610f70e49cda36
Andreas Schmitz <schmitz@occamlabs.de>
2013-03-18 15:25:27 +0100

    added mojo template to configure eclipse working sets

---
+++ src/main/java/org/deegree/maven/EclipseWorkingSetMojo.java
@@ -0,0 +1,81 @@
+//$HeadURL$
+/*----------------------------------------------------------------------------
+ This file is part of deegree, http://deegree.org/
+ Copyright (C) 2001-2011 by:
+ - Department of Geography, University of Bonn -
+ and
+ - lat/lon GmbH -
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ Contact information:
+
+ lat/lon GmbH
+ Aennchenstr. 19, 53177 Bonn
+ Germany
+ http://lat-lon.de/
+
+ Department of Geography, University of Bonn
+ Prof. Dr. Klaus Greve
+ Postfach 1147, 53001 Bonn
+ Germany
+ http://www.geographie.uni-bonn.de/deegree/
+
+ e-mail: info@deegree.org
+ ----------------------------------------------------------------------------*/
+package org.deegree.maven;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * Mojo to automatically setup deegree working sets and to move projects into working sets.
+ * 
+ * @goal setup-eclipse-working-sets
+ * @aggregator
+ * 
+ * @author <a href="mailto:schmitz@occamlabs.de">Andreas Schmitz</a>
+ * @author last edited by: $Author: stranger $
+ * 
+ * @version $Revision: $, $Date: $
+ */
+public class EclipseWorkingSetMojo extends AbstractMojo {
+
+    /**
+     * @parameter default-value="${project}"
+     * @required
+     * @readonly
+     */
+    private MavenProject project;
+
+    @Override
+    public void execute()
+                            throws MojoExecutionException, MojoFailureException {
+        List<MavenProject> modules = project.getCollectedProjects();
+        List<String> modList = new ArrayList<String>();
+        for ( MavenProject p : modules ) {
+            if ( p.getPackaging().equals( "pom" ) ) {
+                continue;
+            }
+            modList.add( p.getArtifactId() );
+        }
+        Collections.sort( modList );
+    }
+
+}
